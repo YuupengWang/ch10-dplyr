ea45f0fc667728932403df4b3ac51d1a74f89098
yupeng wang <244169548@qq.com>
2018-04-20 19:25:40 -0700

    finish exercise

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -32,18 +32,22 @@ cars_1997 <- mutate(cars_1997, average = (hwy+cty) / 2)
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+two_wheel_drive <- filter(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+worest <- select(filter(two_wheel_drive, hwy == min(hwy)), id)
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+best_vehicle <- function(yeara, makes) {
+  time <- filter(vehicles, year == yeara, make == makes)
+  best <- select(filter(time, hwy == max(hwy)), model)
+  return(best)
+}
 
 # What was the most efficient Honda model of 1995?
-
+efficient <- best_vehicle(1995, 'Honda')
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -9,22 +9,27 @@ library(dplyr)
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
-
+dep_delay_by_month <- flights %>% group_by(month) %>% summarise(mean_dep = mean(dep_delay, na.rm = TRUE))
 
 # Which month had the greatest average departure delay?
-
+filter(dep_delay_by_month, mean_dep == max(mean_dep)) %>% select(month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay" in that order), you can create
 # a scatterplot by passing that data frame to the 'plot()' function
-
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
-
-
+highest <- flights %>% group_by(dest) %>% summarise(mean_arrival_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  arrange(-mean_arrival_delay)
+head(highest)
 # You can look up these airports in the `airports` data frame!
 
 
 # Which city was flown to with the highest average speed?
+fasted_speed <- flights %>% mutate(speed = distance/air_time * 60) %>% group_by(dest) %>%
+  summarise(average = mean(speed, na.rm = TRUE)) %>%
+  filter(average == max(avg_speed, na.rm = TRUE)) %>%
+  select(dest)
 
